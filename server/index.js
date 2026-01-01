625c735868aa5f7933fa4b56c91eea421aa67561
Jason <byronjason902@gmail.com>
2026-01-01 09:43:56 +0100

    feat: add auth Routes and Controllers

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 import express from "express";
-
+import dotenv from "dotenv";
+import { connectDb } from "./db/connectDb.js";
+import authRoutes from "./routes/authRoutes.js";
+dotenv.config();
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
-})
-
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+  res.send("Hello World!");
 });
 
+app.use("/api/auth", authRoutes);
+
+app.listen(3000, () => {
+  connectDb();
+  console.log("Server is running on port 3000");
+});
