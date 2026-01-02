0ea7872ebd0153b9b5722a21d9fddba2fa961a42
Jason <byronjason902@gmail.com>
2026-01-02 12:30:56 +0100

    fix: Defined logout in the authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, signup } from "../controllers/authControllers.js";
+import { login, signup, logout } from "../controllers/authControllers.js";
 
 const router = express.Router();
 
