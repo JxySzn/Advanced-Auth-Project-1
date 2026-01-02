8f7a25b1afe4212dbfa4d1dbb8c811064ef02e4d
Jason <byronjason902@gmail.com>
2026-01-02 12:24:41 +0100

    feat: add Databaseconnection controllers

--- server/db/connectDb.js
+++ server/db/connectDb.js
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
 
-
-
 export const connectDb = async () => {
   try {
     console.log("mongodb uri: ", process.env.MONGO_URI);
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import { connectDb } from "./db/connectDb.js";
 import authRoutes from "./routes/authRoutes.js";
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -11,7 +12,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   connectDb();
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port: ", PORT);
 });
