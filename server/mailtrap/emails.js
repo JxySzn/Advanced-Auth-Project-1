9d9120dd5a4d6bd8a18a94c6986728bafdf5e79e
Jason <byronjason902@gmail.com>
2026-01-13 14:59:40 +0100

    Fix: Verification endpoint error

--- server/mailtrap/emails.js
+++ server/mailtrap/emails.js
@@ -8,7 +8,7 @@ export const sendVerificationEmail = async (email, verificationToken) => {
     const response = await mailtrapClient.send({
       from: sender,
       to: recipient,
-      subject: "Verify your email address",
+      subject: "Verify your email ",
       html: VERIFICATION_EMAIL_TEMPLATE.replace(
         "{verificationCode}",
         verificationToken
@@ -17,6 +17,7 @@ export const sendVerificationEmail = async (email, verificationToken) => {
     });
     console.log("Email sent successfully", response);
   } catch (error) {
+    console.error(`Error sending verification`, error);
     throw new Error(`Error sending verification email: ${error}`);
   }
 };
