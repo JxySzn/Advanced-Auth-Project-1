ad02dcad7fa9a4ab26ced6485d06ab5564633836
Jason <byronjason902@gmail.com>
2026-01-14 15:49:25 +0100

    Feat: Add Login Endoint

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -89,9 +89,42 @@ export const verifyEmail = async (req, res) => {
 
 // login  route handler
 export const login = async (req, res) => {
-  res.send("login route");
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+    const isPasswordValid = await bcryptjs.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    generateTokenAndSetCookie(res, user._id);
+    user.lastLogin = new Date();
+    await user.save();
+    res.status(200).json({
+      success: true,
+      message: "Logged in successful",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    console.log("Error In Login", error);
+    res.status(400).json({ success: false, message: error.message });
+  }
 };
 
+// Logout route handler
 export const logout = async (req, res) => {
-  res.send("logout route");
+  res.clearCookie("token");
+  res.status(200).json({ success: true, message: "Loged out successfully" });
 };
